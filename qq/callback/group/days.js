f735df2460df0c6ca999137ccf4ac4ff29706de1
小恸恸 <wtt150249@gmail.com>
2024-07-26 23:18:48 +0800

    feat: 时间设置为上海时间

--- qq/callback/group/days.js
+++ qq/callback/group/days.js
@@ -1,8 +1,11 @@
 const Import = require("../../verify.js");
 const dayjs = require("dayjs");
+var timezone = require("dayjs/plugin/timezone");
 
 const quotesList = require("days-quotes");
 const originDay = dayjs("2024-04-14");
+dayjs.extend(timezone);
+dayjs.tz.setDefault("Asia/Shanghai");
 
 async function doShareDaysQuotes({ qq, groupNo, message, nickName }) {
 	if (message === "/今日日语") {
@@ -15,7 +18,7 @@ async function doShareDaysQuotes({ qq, groupNo, message, nickName }) {
 		Import.sendGroupMessage(groupNo, [
 			{
 				type: "Plain",
-				text: quotesList.list[key].sentence
+				text: quotesList.list[key - 1].sentence
 			}
 		]);
 	}
