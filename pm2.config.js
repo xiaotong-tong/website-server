0cd498de6023ac801cceb2226d8e3ff21627711e
小恸恸 <wtt150249@gmail.com>
2024-12-12 21:30:12 +0800

    feat: 移除qqbot代码为单独仓库

--- pm2.config.js
+++ pm2.config.js
@@ -3,22 +3,11 @@ module.exports = {
 		{
 			name: "server",
 			script: "./index.js"
-		},
+		}
 
 		//		{
 		//			name: "mirai qq worker",
 		//			script: "./qq/verify.js"
 		//		},
-		{
-			name: "qqOfficial worker",
-			script: "./qqOfficial/verify.js"
-		}
-		//		{
-		//			name: "qqOfficial sandbox worker",
-		//			script: "./qqOfficial/verify.js",
-		//			env: {
-		//				sandbox: "true" // 环境变量
-		//			}
-		//		}
 	]
 };
